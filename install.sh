9eb2b6e171f96234f3f0441b280bec36b3ba7cea
Firstruner <contact@firstruner.fr>
2024-07-20 19:54:37 +0200

    Update install for Alma + Ubuntu

--- install.sh
+++ install.sh
@@ -1,26 +1,29 @@
+clear
+echo "[[[   - Version de votre OS -   ]]]"
+echo
+echo "1 - Ubuntu 22.04"
+echo "2 - Alma Linux 9 (Version minimal)"
+echo
+read -p "Qu'elle est votre version ?" osversion
+echo
+echo
+echo "[[[   - Préparation et récupération des scripts -   ]]]"
+echo
+if [[ $osversion = 1 ]]; then
+  script_dir="Ubuntu_22_04"
+  sudo apt-get update
+  sudo apt-get install -y git gparted
+else
+  script_dir="AlmaLinux_9"
+  dnf update -y
+  sudo apt-get install git gparted -y
+fi
+echo
 echo "[[[   - Récupération de Git -   ]]]"
 echo
-sudo apt-get update
-sudo apt-get install -y git gparted
 cd $HOME
 git clone https://github.com/firstruner/scripts_devops.git
-clear
-echo
-echo
-echo "[[[   - PARAMETRAGE DU SYSTEME -   ]]]"
-echo
-read -p "-->> Voulez-vous ajuster la taille de la partition [y/n] ? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy] ]]; then
-  gparted
-else
-  echo " (i) : Il vous est possible à tout moment de lancer l'outils avec la commande gparted"
-fi
-echo
-read -p "-->> Voulez-vous installer Docker maintenant [y/n] ? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy] ]]; then
-  bash $HOME/scripts_devops/installdocker.sh
-else
-  echo " (i) : L'installation peux se faire manuellement ou en lançant le script via : bash $HOME/scripts_devops/installdocker.sh"
-fi
+cd scripts_devops
+export script_dir
+export osversion
+bash install.sh
