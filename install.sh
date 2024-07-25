95826ec8063e2bf7da1dc739aa4fd87504ae12c9
Firstruner <contact@firstruner.fr>
2024-07-25 14:46:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 clear
-echo
-echo "[[[   - Verification systeme -   ]]]"
-echo
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -10,15 +7,19 @@ MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 NC='\033[0m'
+clear
+echo
+echo -e "${CYAN}[[[   - Verification systeme -   ]]]${NC}"
+echo
 cpucount=$((lscpu | egrep 'Processeur\(s\)|CPU\(s\)' | awk -F: '{print $2}' | head -n 1) | sed 's/^[ \t]*//;s/[ \t]*$//')
 if [[ $cpucount -gt 1 ]]; then
-   echo "Votre machine a le nombre minimal de CPU requis"
+   echo -e "${GREEN}Votre machine a le nombre minimal de CPU requis${NC}"
 else
-   echo "Installtation stoppée : Votre machine ne dispose pas de 2 CPU !"
+   echo -e "${RED}Installtation stoppée : Votre machine ne dispose pas de 2 CPU !${NC}"
    exit 1;
 fi
 echo
-echo "[[[   - Version de votre OS -   ]]]"
+echo -e "${CYAN}[[[   - Version de votre OS -   ]]]${NC}"
 echo
 echo "1 - Ubuntu 22.04"
 echo "2 - Alma Linux 8/9 (Version minimale)"
@@ -26,7 +27,7 @@ echo
 read -p "Qu'elle est votre version ?" -n 1 -r osversion
 echo
 echo
-echo "[[[   - Préparation et récupération des scripts -   ]]]"
+echo -e "${CYAN}[[[   - Préparation et récupération des scripts -   ]]]${NC}"
 echo
 if [[ $osversion = 1 ]]; then
   script_dir="Ubuntu_22_04"
@@ -38,7 +39,7 @@ else
   sudo dnf install git parted -y
 fi
 echo
-echo "[[[   - Récupération de Git -   ]]]"
+echo -e "${CYAN}[[[   - Récupération de Git -   ]]]${NC}"
 echo
 cd $HOME
 git clone https://github.com/firstruner/scripts_devops.git
